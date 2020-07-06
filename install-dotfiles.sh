e35beac11e0a7b6372085d38fc04c8fbe97118eb
Ryan Bottoms <rkbottoms@users.noreply.github.com>
2020-07-06 10:08:42 -0500

    update script to install dotfiles

---
+++ install-dotfiles.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+#install dotfiles
+cp ~/dotfiles/.bash_prompt ~/.bash_prompt
+cp ~/dotfiles/.bashrc ~/.bashrc
+cp ~/dotfiles/.gitconfig ~/.gitconfig
+cp ~/dotfiles/.git-prompt.sh ~/.git-prompt.sh
+cp ~/dotfiles/general.aliases ~/general.aliases
