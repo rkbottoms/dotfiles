8f3a7f910505f9061c66182201efe589df95719b
Ryan Bottoms <rkbottoms@users.noreply.github.com>
2020-10-31 11:56:00 -0500

    Change author

--- install-dotfiles.sh
+++ install-dotfiles.sh
@@ -5,8 +5,5 @@ cp .bash_prompt ~/.bash_prompt
 cp .bashrc ~/.bashrc
 cp .gitconfig ~/.gitconfig
 cp .git-prompt.sh ~/.git-prompt.sh
-cp general.aliases ~/general.aliases
-<<<<<<< HEAD
+cp .general.aliases ~/general.aliases
 cp .fonts ~/.fonts
-=======
->>>>>>> f25722e0ae5f975c5ed5a68797884ec52e9a7edf
